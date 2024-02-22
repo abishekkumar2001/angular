ff5116a0711a3e968cbf0770b94fb455f8e31d0e
abishekkumar23 <abishekkumar.k@datasirpi.com>
2024-02-22 10:07:16 +0530

    Conditional Rendering in Angular

---
+++ conditional-rendering/src/app/conditionals/conditionals.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-conditionals',
+  templateUrl: './conditionals.component.html',
+  styleUrls: ['./conditionals.component.css']
+})
+export class ConditionalsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  clicked = true;
+  month=0;
+
+}
